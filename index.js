7b46ad453a82ab0d384b71b2e903fd6a9dcfce35
Meghann Stamper <meg.stamp@yahoo.com>
2023-04-03 15:52:26 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const PORT = 3030;
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send("I am index");
+});
+
+app.get('/hello', (req, res) => {
+    res.send("Hello there");
+});
+
+app.listen(PORT, () => {
+    console.log(`listening on port http://localhost:${PORT}.`);
+});
+
+
+
